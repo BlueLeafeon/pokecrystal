b86f8789f0d1a93d6775296677355d0ec4244cc5
Leafy <shinybluepikachu@gmail.com>
2024-10-22 21:51:27 -0400

    Kurt now finishes balls instantly.

--- maps/KurtsHouse.asm
+++ maps/KurtsHouse.asm
@@ -72,8 +72,8 @@ Kurt1:
 	iffalse .NoRoomForBall
 	setevent EVENT_KURT_GAVE_YOU_LURE_BALL
 .GotLureBall:
-	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
-	iftrue .WaitForApricorns
+;	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
+;	iftrue .WaitForApricorns
 	checkevent EVENT_GAVE_KURT_RED_APRICORN
 	iftrue .GiveLevelBall
 	checkevent EVENT_GAVE_KURT_BLU_APRICORN
@@ -167,11 +167,19 @@ Kurt1:
 
 .GaveKurtApricorns:
 	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
-	setflag ENGINE_KURT_MAKING_BALLS
-.WaitForApricorns:
-	writetext KurtsHouseKurtItWillTakeADayText
+;	setflag ENGINE_KURT_MAKING_BALLS
+;.WaitForApricorns:
+	writetext KurtsHouseKurtGetStartedText
 	waitbutton
+;	writetext KurtsHouseKurtItWillTakeADayText
+;	waitbutton
 	closetext
+	special FadeOutToBlack
+	special ReloadSpritesNoPalettes
+	playsound SFX_WARP_TO
+	waitsfx
+	pause 35
+	sjump Kurt1
 	end
 
 .Cancel:
@@ -528,13 +536,19 @@ KurtsHouseKurtAskYouHaveAnApricornText:
 	line "into a BALL."
 	done
 
-KurtsHouseKurtItWillTakeADayText:
-	text "KURT: It'll take a"
-	line "day to make you a"
-
-	para "BALL. Come back"
-	line "for it later."
+KurtsHouseKurtGetStartedText:
+	text "Kurt: I'll get"
+	line "started right now!"
 	done
+
+
+;KurtsHouseKurtItWillTakeADayText:
+;	text "KURT: It'll take a"
+;	line "day to make you a"
+;
+;	para "BALL. Come back"
+;	line "for it later."
+;	done
 
 KurtsHouseKurtThatsALetdownText:
 	text "KURT: Oh…"
